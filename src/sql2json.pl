1bfa678e1cb8c461536dcbaab880cfc1911ea6d2
Xavier Riley <xavriley@hotmail.com>
2015-04-29 09:25:54 +0100

    WIP: Add 2014 spreadsheet data and extra instructions in README. Data not quite right yet.

--- src/sql2json.pl
+++ src/sql2json.pl
@@ -13,7 +13,7 @@ my $json_file = shift or die "Failed to provide output json filename";
 my $dbh = DBI->connect("dbi:SQLite:dbname=$db_file", "", "");
 
 sub read_database() {
-    my $query = "SELECT project.hmt_id,project.name as name,department.name as dept_name,authority.name as auth_name,sector.name as sector_name,constituency.name as constituency_name,region.name as region_name, status, date_ojeu, date_pref_bid, date_fin_close, date_cons_complete, date_ops, contract_years, off_balance_IFRS, off_balance_ESA95, off_balance_GAAP, capital_value, spv.name from project,department,authority,sector,constituency,region,spv WHERE project.department = department.id AND project.authority = authority.id AND project.sector = sector.id AND project.constituency = constituency.id AND project.region = region.id AND project.spv = spv.spv_id GROUP BY project.hmt_id";
+    my $query = "SELECT project.hmt_id,project.name as name,department.name as dept_name,authority.name as auth_name,sector.name as sector_name,constituency.name as constituency_name,region.name as region_name, status, date_ojeu, date_pref_bid, date_fin_close, date_cons_complete, date_ops, contract_years, off_balance_IFRS, off_balance_ESA95, off_balance_GAAP, capital_value, spv.name from project,department,authority,sector,constituency,region,spv WHERE project.department_id = department.id AND project.authority_id = authority.id AND project.sector_id = sector.id AND project.constituency_id = constituency.id AND project.region_id = region.id AND project.spv_id = spv.spv_id GROUP BY project.hmt_id";
     $projects = $dbh->selectall_arrayref($query);
 };
 
